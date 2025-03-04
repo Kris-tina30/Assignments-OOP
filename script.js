ccab921cd363c91e30f42373586c19f96890fafe
Kris-tina30 <kristina.pichkar.15@gmail.com>
2025-03-04 21:17:27 +0100

    add promise assignment, delete 2 js files, left one

--- script.js
+++ script.js
@@ -26,3 +26,40 @@ console.log(person1.introduce());
 
 const person2 = new Person("Alice", 30);
 console.log(person2.introduce());
+
+// Exercise 2 - This
+
+// Create a new object using the `Person` class you defined in Exercise 1.
+// Name the object `person1` and set the `name` and `age` properties accordingly.
+
+// Then, create a function called `describePerson` that takes a callback function as
+// an argument. Inside the `describePerson` function, call the callback function with `
+// person1` as the `this` context.
+
+const person3 = new Person("Kristina", 32);
+
+function describePerson(callback) {
+  return callback.call(person3);
+}
+
+describePerson(person3.introduce);
+console.log(describePerson(person3.introduce));
+
+//Exercise 3 - Promises
+
+// Write a function called `wait` that takes a number as an argument
+
+// (representing milliseconds) and returns a Promise. The Promise should
+
+// resolve after the given number of milliseconds, using `setTimeout`.
+
+// The resolved value can be anything you choose.
+
+function wait(milliseconds) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Resolved after ${milliseconds} milliseconds`);
+    }, milliseconds);
+  });
+}
+wait(2000).then((message) => console.log(message));
